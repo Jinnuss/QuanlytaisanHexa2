89a861f3ed8a5bdf7939d0e6242a849ab007a174
trungnguyen <b28082002@gmail.com>
2026-07-14 11:30:51 +0700

    qr fix1

--- src/pages/PublicAssetDetail.js
+++ src/pages/PublicAssetDetail.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { get, ref } from "firebase/database";
-import { db } from "../firebase";
 import AuditLog from "../components/AuditLog";
 import { getPublicAsset } from "../assetService";
 
